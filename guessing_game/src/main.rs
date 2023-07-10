9915b3d6ccc722861223811ef48c3f90c23bdba2
jackmac <jackmaca@msn.com>
2023-07-10 21:03:17 +0930

    added rand num generation

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,17 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Enter your guess");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     let mut guess = String::new();
 
+    println!("The secret num is {secret_number}");
+
     io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    .read_line(&mut guess)
+    .expect("Failed to read line");
 
     println!("You guessed: {guess}");
 }
