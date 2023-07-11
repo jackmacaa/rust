8efe3a1129bb2af2b81f518297785a4723d4a2ef
jackmac <jackmaca@msn.com>
2023-07-11 21:56:02 +0930

    added match,  convert str to int and loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,17 +1,27 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Enter your guess");
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    let mut guess = String::new();
+   
+    loop {
+        println!("Enter your guess");
 
-    println!("The secret num is {secret_number}");
+        let mut guess = String::new();
 
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
+    }
 }
